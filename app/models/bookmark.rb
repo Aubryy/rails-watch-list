5eaa9fc624f2b181160c793d58a9fe16ff3ac37b
Aubry Louvet <aubry.louvet@gmail.com>
2023-05-26 11:27:57 +0200

    db create and migrate : ok

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,8 +5,5 @@ class Bookmark < ApplicationRecord
   # A bookmark must be linked to a movie and a list, and the [movie, list] pairings should be unique.
 
   validates :comment, length: { minimum: 6 }
-  # validates_uniqueness_of :id, scope: [:movie_id, :list_id]
-  # validates_presence_of [:movie_id, :list_id]
-
   validates :movie, uniqueness: { scope: :list }
 end
